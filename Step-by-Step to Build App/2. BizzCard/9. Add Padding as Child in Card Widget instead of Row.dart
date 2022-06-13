95d36104f6766b18d53e7582c684bb0fae28bc0a
FutureIntelligenceDxB <106425118+FutureIntelligenceDxB@users.noreply.github.com>
2022-06-13 14:26:56 +0530

    Create 9. Add Padding as Child in Card Widget instead of Row.dart

---
+++ Step-by-Step to Build App/2. BizzCard/9. Add Padding as Child in Card Widget instead of Row.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey[500],
+        body: SafeArea(
+          child: Column(
+            children: [
+              const CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.transparent,
+                backgroundImage: AssetImage('images/FI.png'),
+              ),
+              const Text(
+                'Future Intelligence',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.0,
+                  fontSize: 25.0,
+                  color: Colors.white,
+                  fontFamily: 'kalam',
+                ),
+              ),
+              Text(
+                'Every Child. Future Ready.',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                  fontSize: 20.0,
+                  color: Colors.grey[200],
+                  fontFamily: 'Source Sans Pro',
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 50.0),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 50.0),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.phone,
+                        color: Colors.grey[500],
+                      ),
+                      const SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        '+971 55 9835 002',
+                        style: TextStyle(
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20.0,
+                          color: Colors.grey[700],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 50.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.mail,
+                      color: Colors.grey[500],
+                    ),
+                    const SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'hello@futureintelligence.me',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                        color: Colors.grey[700],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
