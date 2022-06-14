aa6d1dc97efa01d8630269139a8aeecb01b67c48
FutureIntelligenceDxB <106425118+FutureIntelligenceDxB@users.noreply.github.com>
2022-06-14 18:25:22 +0530

    Create 12. Import math library & Add setState Property in TextButton Widget.dart

---
+++ Step-by-Step to Build App/3. Dice/12. Import math library & Add setState Property in TextButton Widget.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        appBar: AppBar(
+          title: const Text('Dice'),
+          backgroundColor: Colors.teal.shade500,
+        ),
+        body: const DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  leftDiceNumber = Random().nextInt(6) + 1;
+                  rightDiceNumber = Random().nextInt(6) + 1;
+                  debugPrint('diceNumber = $leftDiceNumber');
+                });
+              },
+              child: Image.asset(
+                'images/dice$leftDiceNumber.png',
+              ),
+            ),
+          ),
+          Expanded(
+            child: TextButton(onPressed: () {
+              setState(() {
+                leftDiceNumber = Random().nextInt(6) + 1;
+                rightDiceNumber = Random().nextInt(6) + 1;
+                debugPrint('diceNumber= $rightDiceNumber');
+              });
+            },
+              child: Image.asset(
+                'images/dice$rightDiceNumber.png',
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
