09cefb805d93c0e48ffb3f17874a19f20f90adb7
FutureIntelligenceDxB <106425118+FutureIntelligenceDxB@users.noreply.github.com>
2022-06-14 17:31:10 +0530

    Create 01. Add Image to Stateless Widget.dart

---
+++ Step-by-Step to Build App/3. Dice/01. Add Image to Stateless Widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        appBar: AppBar(
+          title: const Text('Dice'),
+          backgroundColor: Colors.teal.shade500,
+        ),
+        body: const DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: const [
+        Image(
+          image: AssetImage('images/dice1.png'),
+        ),
+      ],
+    );
+  }
+}
