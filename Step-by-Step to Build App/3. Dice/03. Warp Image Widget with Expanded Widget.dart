9240bf19fb14323a71e4053b007cfa7f98ff4c8a
FutureIntelligenceDxB <106425118+FutureIntelligenceDxB@users.noreply.github.com>
2022-06-14 17:43:17 +0530

    Create 03. Warp Image Widget with Expanded Widget.dart

---
+++ Step-by-Step to Build App/3. Dice/03. Warp Image Widget with Expanded Widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        appBar: AppBar(
+          title: const Text('Dice'),
+          backgroundColor: Colors.teal.shade500,
+        ),
+        body: const DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: const [
+        Expanded(
+          child: Image(
+            image: AssetImage(
+              'images/dice1.png',
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
