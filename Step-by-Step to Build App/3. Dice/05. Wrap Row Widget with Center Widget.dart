8f55775106a7da869132db9c571f5e4ae923dbad
FutureIntelligenceDxB <106425118+FutureIntelligenceDxB@users.noreply.github.com>
2022-06-14 17:52:55 +0530

    Create 05. Wrap Row Widget with Center Widget.dart

---
+++ Step-by-Step to Build App/3. Dice/05. Wrap Row Widget with Center Widget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        appBar: AppBar(
+          title: const Text('Dice'),
+          backgroundColor: Colors.teal.shade500,
+        ),
+        body: const DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Image.asset(
+              'images/dice1.png',
+            ),
+          ),
+          Expanded(
+            child: Image.asset(
+              'images/dice1.png',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
