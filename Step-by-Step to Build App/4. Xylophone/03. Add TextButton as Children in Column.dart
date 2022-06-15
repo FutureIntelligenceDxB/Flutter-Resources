a7a51cc7d62784a537dbf1dea89915abe29edb18
FutureIntelligenceDxB <106425118+FutureIntelligenceDxB@users.noreply.github.com>
2022-06-15 12:59:10 +0530

    Create 03. Add TextButton as Children in Column.dart

---
+++ Step-by-Step to Build App/4. Xylophone/03. Add TextButton as Children in Column.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(const Xylophone());
+}
+
+class Xylophone extends StatelessWidget {
+  const Xylophone({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              TextButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateColor.resolveWith((states) => Colors.pink.shade300),
+                ),
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note1.wav');
+                },
+                child: const Text(
+                  '',
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
