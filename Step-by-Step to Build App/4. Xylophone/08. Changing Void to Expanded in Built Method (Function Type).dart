ddd07ec6b5b7dab23cb33da5becfedcc86a6089b
FutureIntelligenceDxB <106425118+FutureIntelligenceDxB@users.noreply.github.com>
2022-06-15 13:28:41 +0530

    Create 08. Changing Void to Expanded in Built Method (Function Type).dart

---
+++ Step-by-Step to Build App/4. Xylophone/08. Changing Void to Expanded in Built Method (Function Type).dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(const Xylophone());
+}
+
+class Xylophone extends StatelessWidget {
+  const Xylophone({Key? key}) : super(key: key);
+
+  void playSound(int soundNo) {
+    final player = AudioCache();
+    player.play('note$soundNo.wav');
+  }
+
+  Expanded builtKey(){
+    return Expanded(
+      child: TextButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateColor.resolveWith((states) => Colors.pink.shade300),
+        ),
+        onPressed: () {
+          playSound(1);
+        },
+        child: const Text(
+          '',
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              builtKey(),
+              builtKey(),
+              builtKey(),
+              builtKey(),
+              builtKey(),
+              builtKey(),
+              builtKey(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
